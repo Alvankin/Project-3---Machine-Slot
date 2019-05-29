4e32552015fd25fc3a8b564f84f34392e8785344
Alvin Yan Sheng LOW <AlvinYanSheng.LOW@Subsea7.com>
2019-05-29 22:14:17 +0200

    Initiation of Chapter 9

---
+++ Code.R
@@ -0,0 +1,12 @@
+# Project 3 : Slot Machine----
+# Chapter 9 Programs----
+get_symbols <- function() {
+  wheel <- c("DD", "7", "BBB", "BB", "B", "C", "0")
+  sample(
+    wheel,
+    size = 3,
+    replace = TRUE,
+    prob = c(0.03, 0.03, 0.06, 0.1, 0.25, 0.01, 0.52)
+  )
+}
+get_symbols()
